80aa364c4f90024af64c6b1fda90088829ed7fa9
kissenger <ivyterrace@hotmail.co.uk>
2023-10-08 18:24:30 +0100

    refactor horz scroll for performance

--- src/app/main/15-feed/feed.component.ts
+++ src/app/main/15-feed/feed.component.ts
@@ -18,9 +18,12 @@ export class FeedComponent implements OnInit, OnDestroy, AfterViewChecked{
   @ViewChild('captionBox') captionBox: ElementRef | undefined;
 
   private httpSubs: Subscription | undefined;
+  private screenSizeSubs: Subscription | undefined;
   public instaFeed: Array<{caption: string, media_url: string, permalink: string, timestamp: string}> = []
   private qty = 5;
   public nLines = 0;
+  private dWidth = 0;
+  private dHeight = 0;
 
   constructor(
     private http: HttpService,
@@ -32,8 +35,10 @@ export class FeedComponent implements OnInit, OnDestroy, AfterViewChecked{
     ) {
     this.nLines = this.getCaptionBoxLines();
 
-      this.screenSize.resize.subscribe( () => {
+      this.screenSizeSubs = this.screenSize.resize.subscribe( () => {
+        this.getDimensions();
       });
+
       this.httpSubs = this.http.getInstaPosts().subscribe({
         next: (result: any) => {
           this.instaFeed = result.data.map( (m: any) => m).slice(0, this.qty);
@@ -50,25 +55,31 @@ export class FeedComponent implements OnInit, OnDestroy, AfterViewChecked{
 
     window.addEventListener("touchmove", (e) => {
       this.horizontalScroll();
-
+      this.setCookie();
     });
 
 
     window.addEventListener("scroll", (e) => {
       this.horizontalScroll();
+      this.setCookie();
     });
   }
 
   ngAfterViewChecked() {
     this.nLines = this.getCaptionBoxLines();
     this.changeDetection.detectChanges();
+    this.getDimensions();
+
   }
 
-  ngOnDestroy(): void {
-   if (this.httpSubs) { this.httpSubs.unsubscribe(); }
+  getDimensions() {
+    // get available space for vertical and horizontal scrolling based on layout
+    this.dHeight = this.stickyParent?.nativeElement.offsetHeight - this.stickyChild?.nativeElement.offsetHeight;
+    this.dWidth = this.scrollBox?.nativeElement.offsetWidth - this.stickyParent?.nativeElement.offsetWidth ;
   }
 
   getCaptionBoxLines() {
+    // determine number of lines of caption to allow based on screen height
     if (this.captionBox && this.stickyChild) {
       const cssLineHeight = getComputedStyle(<HTMLElement>this.captionBox.nativeElement).getPropertyValue("line-height");
       const lineHight = parseFloat(cssLineHeight.substring(0, cssLineHeight.length-2));
@@ -80,23 +91,26 @@ export class FeedComponent implements OnInit, OnDestroy, AfterViewChecked{
   }
 
   horizontalScroll() {
+    // move the scroll-box left and right according to screen position
+    const dScroll = this.stickyParent?.nativeElement.getBoundingClientRect().bottom - this.stickyChild?.nativeElement.getBoundingClientRect().bottom;
+    const scrollPosition = this.dWidth * (1 - dScroll / this.dHeight);
+    this.renderer.setStyle(this.scrollBox?.nativeElement, 'transform', `translate3d(${scrollPosition < 0 ? 0 : -scrollPosition}px, 0, 0)`);
+  }
+
+  setCookie() {
     // set visit time if insta posts are in view
     const check = this.stickyParent?.nativeElement.getBoundingClientRect().top - this.stickyChild?.nativeElement.getBoundingClientRect().top;
     if (check !== 0) {
       this.auth.setVisitTime();
     }
-
-    // set up horizontal scroll section
-    if (this.scrollBox && this.stickyParent && this.stickyChild && this.captionBox) {
-      const dHeight = this.stickyParent.nativeElement.offsetHeight - this.stickyChild.nativeElement.offsetHeight;
-      const dScroll = this.stickyParent.nativeElement.getBoundingClientRect().bottom - this.stickyChild.nativeElement.getBoundingClientRect().bottom;
-      const scrollWidth = this.scrollBox.nativeElement.getBoundingClientRect().width - this.stickyParent.nativeElement.offsetWidth ;
-      const scrollPosition = scrollWidth * (1 - dScroll/dHeight);
-      this.renderer.setStyle(this.scrollBox.nativeElement, 'transform', `translate3d(${scrollPosition < 0 ? 0 : -scrollPosition}px, 0, 0)`);
-      console.log(dScroll,scrollWidth,scrollPosition)
-    }
   }
 
+
+
+  ngOnDestroy(): void {
+    this.screenSizeSubs?.unsubscribe();
+    this.httpSubs?.unsubscribe();
+   }
 
 }
 
